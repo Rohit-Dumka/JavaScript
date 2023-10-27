1189203d47c47fb6f91f5c593b3abd7023744d6f
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-10-27 17:17:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+console.log(3+3)
+console.log("Rohit");
+console.log(3+
+    5) //code readability should be high
+
+let name = "Rohit"
+let age = 23
+let isLoggedIn = false
+let state
+let temp = null //null : empty
+
+
+//number 
+//bigint
+//string => "" ''
+//boolean => true/false
+//null  : standalone value
+//undefined 
+//symbol => unique (used mostly in react)
+//object
+
+console.log(typeof "Rohit");
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
