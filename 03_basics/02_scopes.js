90f399e4ce5add45ccce39420755cc8b094b4278
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-10-29 13:08:32 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,10 +10,53 @@ if(true){
     const b=20
     var c=30
     // c=30 //still same 
-    console.log("Inner:",a);
+    // console.log("Inner:",a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+
+function one(){
+    const username ="Rohit"
+
+    function two(){ //closure : inner function can accesss variables of outer function
+        const website ="github"
+        // console.log(username); 
+    }
+    // console.log(website); //error out of scope
+
+    two() 
+}
+
+one()
+
+if(true){
+    const username="rohit"
+    if(username === "rohit"){
+        const website = "github"
+        // console.log(username + website);
+    }
+    // console.log(website); //error oos
+}
+
+// console.log(username); error oos
+
+
+// ************************* interesting ***************************
+
+
+console.log(addone(5));
+
+function addone(num){
+    return num +1
+}
+
+
+
+console.log(addTwo(5)); //can't access before initiallization : Hoisting
+
+const addTwo = function(num){  //function or expression
+    return num+2
+}
