271143a325625eac9fe917f51526aa77ed655211
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-10-30 17:42:53 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,55 @@
+//if
+
+const isUserLoggedIn =true
+const temp = 20
+
+// if(temp>50){ 
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// comparison operators ----
+// <, >, <=, >=, ==, !=, === (strict checking), !== 
+
+
+// const score =200
+// if(score>100){
+//     let power ="fly"
+//     console.log(`User power : ${power}`);
+// }
+// console.log(`User power : ${power}`);
+
+
+
+//shorthand notation
+
+
+const balance =1000
+
+// if(balance>500) console.log("yes") //implicit scope
+
+// if(balance>500) console.log("yes"), 
+// console.log("yes again");
+
+// if(balance<500){
+//     console.log("less");
+// }else if (balance< 750){
+//     console.log("less than 750");
+// }else if(balance< 900){
+//     console.log("less than 900");
+// }else   console.log("greater than 900");
+
+
+const UserLoggedIn = true
+const debitCard =true
+const loggedInFromGoogle =false
+const loggedInFromEmail =true
+
+// if(UserLoggedIn && debitCard  && 2==3){
+//     console.log("allow to buy");
+// }
+
+// if(loggedInFromEmail || loggedInFromGoogle) 
+//     console.log("user logged in");
+
