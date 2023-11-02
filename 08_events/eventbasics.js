b84747c96df63b7c25942e38b170b60cc7887c3f
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-11-02 14:13:32 +0000

    basic

---
+++ 08_events/eventbasics.js
@@ -0,0 +1,13 @@
+javascript --
+        Synchronous
+        Single Threaded
+
+Execution Context --
+        execute one line of code at a time 
+        (each execution waits for the last one to complete before executing)
+
+Blocking Code               V/S         Non Blocking Code
+                            |                    
+block the flow of program   |        does not block execution
+                            |                                        
+read file sync              |           read file async
